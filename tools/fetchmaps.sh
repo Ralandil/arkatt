778d14dba3377963cc481b3b54fd40aa74602d15
eld <adam@konczynski.com>
2016-08-22 13:40:03 +0100

    map updater

--- tools/fetchmaps.sh
+++ tools/fetchmaps.sh
@@ -5,12 +5,12 @@ wget http://arkadia.rpg.pl/klient/web_client/maps/ -r -l 1
 
 ls arkadia.rpg.pl/klient/web_client/maps/*.map > maplist.list
 
-suffix="..............................................................................................................................................................."
+suffix="..................................................."
 
 while read line
 do
 	newname=`echo $line|tr -d " "`
-	iconv -f utf-8 -t US-ASCII//TRANSLIT -c "$line" |sed 's/ /./g' > $newname.ascii
+	iconv -f utf-8 -t US-ASCII//TRANSLIT -c "$line" |sed 's/ /./g'|sed 's/\\/L/g' > $newname.ascii
 	rm "$line"
 done < maplist.list
 
@@ -20,6 +20,9 @@ ls arkadia.rpg.pl/klient/web_client/maps/*.ascii > maplist.list
 while read line
 do
 	newname=`echo $line|sed 's/.ascii//g'`
-	while read ll; do echo "${ll}${suffix}" >> $newname; done < $line
+	while read ll; do 
+		echo "${ll}${suffix}"|tr -d '\n' >> $newname
+		printf "\n" >> $newname
+	done < $line
 done < maplist.list
 
